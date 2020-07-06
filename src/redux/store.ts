6355694c540b2dc2c60a305ff01b3b0173065c98
nvdai2401 <nvdai2401@gmail.com>
2020-07-06 11:31:36 +0700

    setup redux-logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { persistStore } from 'redux-persist'
 import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './root.reducer'
 import rootSaga from './root.saga'
 
+const logger = createLogger()
 const sagaMiddleware = createSagaMiddleware()
-const middleware = [sagaMiddleware]
+const middleware = []
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger)
 }
-export const store = createStore(rootReducer, applyMiddleware(...middleware))
+export const store = createStore(rootReducer, applyMiddleware(...middleware, sagaMiddleware))
 sagaMiddleware.run(rootSaga)
 export const persistor = persistStore(store)
