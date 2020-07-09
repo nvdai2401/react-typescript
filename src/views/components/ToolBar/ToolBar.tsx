feb123be7b4e6d5820fa0c21cb92fe90e26b16ce
nvdai2401 <nvdai2401@gmail.com>
2020-07-09 19:39:54 +0700

    add icon button in ToolBar

--- src/views/components/ToolBar/ToolBar.tsx
+++ src/views/components/ToolBar/ToolBar.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import clsx from 'clsx'
-import { Grid, Box, Button } from '@material-ui/core'
+import { Grid, Box, Button, IconButton } from '@material-ui/core'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import SearchIcon from '@material-ui/icons/Search'
+import VisibilityIcon from '@material-ui/icons/Visibility'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    alignItems: 'center',
     height: 64,
     position: 'fixed',
     top: 0,
@@ -29,7 +37,17 @@ const ToolBar: React.FC<ToolBarProps> = (props: ToolBarProps) => {
     <Grid container className={classes.root}>
       <Grid item>Total: 500</Grid>
       <Grid item>Select time range</Grid>
-      <Grid item>Group btn</Grid>
+      <Grid item>
+        <IconButton>
+          <EventNoteIcon />
+        </IconButton>
+        <IconButton>
+          <VisibilityIcon />
+        </IconButton>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Grid>
       <Grid item>Button</Grid>
     </Grid>
   )
