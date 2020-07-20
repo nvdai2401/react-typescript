d6cd690ffc12fadfc595af5b8d49089d60632446
nvdai2401 <nvdai2401@gmail.com>
2020-07-20 21:53:57 +0700

    develop UI for Auth Page completely

--- src/views/Auth/Auth.tsx
+++ src/views/Auth/Auth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Grid, Button, Typography, TextField } from '@material-ui/core'
+import { Box, Button, Grid, TextField, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -7,11 +7,55 @@ const useStyles = makeStyles((theme) => ({
     width: '100vw',
     height: '100vh',
     flexWrap: 'nowrap',
+    backgroundColor: theme.palette.background.paper,
+    padding: '100px 16px',
+  },
+  item__inputGroup: {
+    width: '100%',
   },
   item__input: {
     width: '100%',
+    marginTop: theme.spacing(1),
   },
-  [theme.breakpoints.up('md')]: {},
+  item__groupButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    marginTop: theme.spacing(2),
+  },
+  googleButton: {
+    marginTop: theme.spacing(1.5),
+    backgroundColor: 'rgb(66, 133, 244)',
+  },
+  forgotPassword: {
+    alignSelf: 'flex-end',
+    color: theme.palette.secondary.light,
+    marginTop: theme.spacing(1),
+    cursor: 'pointer',
+    fontWeight: 600,
+    lineHeight: 1,
+    '&:hover': {
+      color: theme.palette.secondary.main,
+    },
+  },
+  suggestion: {
+    marginTop: theme.spacing(2),
+    cursor: 'pointer',
+    fontWeight: 600,
+    '& span': {
+      color: theme.palette.secondary.light,
+      textDecoration: 'underline',
+    },
+  },
+  [theme.breakpoints.up('md')]: {
+    root: {
+      margin: '200px auto',
+      width: 400,
+      height: 'auto',
+      padding: theme.spacing(2),
+      borderRadius: 4,
+    },
+  },
 }))
 
 const Auth: React.FC<any> = () => {
@@ -19,7 +63,7 @@ const Auth: React.FC<any> = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const [screen, setScreen] = useState('sign-in')
   return (
     <Grid
       container
@@ -27,10 +71,12 @@ const Auth: React.FC<any> = () => {
       alignItems="center"
       className={classes.root}
     >
-      <Typography variant="h4" gutterBottom>
-        Login
+      <Typography variant="h4">
+        <Box fontWeight={600}>
+          {screen === 'sign-in' ? 'Sign In' : 'Register'}
+        </Box>
       </Typography>
-      <Grid item xs={12}>
+      <Grid item className={classes.item__inputGroup}>
         <TextField
           label="Email"
           type="email"
@@ -46,14 +92,35 @@ const Auth: React.FC<any> = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </Grid>
-      <Grid item xs={12}>
-        <Button variant="contained" color="primary">
-          Login
+      {screen === 'sign-in' ? (
+        <Typography variant="subtitle1" className={classes.forgotPassword}>
+          Forgot password
+        </Typography>
+      ) : null}
+      <Grid item className={classes.item__groupButton}>
+        <Button variant="contained" color="primary" size="large">
+          Sign In
         </Button>
-        <Button variant="contained" color="secondary">
-          Login with Google
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          className={classes.googleButton}
+        >
+          Connect with Google
         </Button>
       </Grid>
+      {screen === 'sign-in' ? (
+        <Typography variant="subtitle1" className={classes.suggestion}>
+          Don’t have an account?{' '}
+          <span onClick={() => setScreen('sign-up')}>Register</span>
+        </Typography>
+      ) : (
+        <Typography variant="subtitle1" className={classes.suggestion}>
+          Have you an account?{' '}
+          <span onClick={() => setScreen('sign-in')}>Sign In</span>
+        </Typography>
+      )}
     </Grid>
   )
 }
